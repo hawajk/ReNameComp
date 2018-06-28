b987d67a2384bcf33cf5b72a44d54e3248a1da58
hawajk <jjkwangzheng@126.com>
2018-06-28 10:27:06 +0800

    Add files via upload

---
+++ RenameComp.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class RenameComp : MonoBehaviour {
+
+    public string szPath;
+    public string szNameReplaceOld;
+    public string szNameReplaceNew;
+    
+	// Use this for initialization
+	void Start () {
+        ReName();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ReName()
+    {
+        string selectPath = Application.dataPath + szPath;
+       
+        //var path_BGImage = Application.dataPath + "/../../BGImage/";
+        if (!Directory.Exists(selectPath))
+        {
+            Debug.LogError(selectPath + "path is not exist!!");
+            return;
+        }
+        var directionBGImage = new DirectoryInfo(selectPath);
+
+        //获取文件信息
+        FileInfo[] files = directionBGImage.GetFiles("*", SearchOption.AllDirectories);
+        
+        for (int i = 0; i < files.Length; i++)
+        {
+            string name = files[i].Name.Replace(szNameReplaceOld, szNameReplaceNew);
+            files[i].MoveTo(selectPath + name);
+            Debug.Log("rename to " + name);
+        }
+
+
+        Resources.UnloadUnusedAssets();
+        System.GC.Collect();
+    }
+}
